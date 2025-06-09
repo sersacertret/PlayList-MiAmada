f1f4b34662748f145550276476f975bf21f1c004
sersacertret <sersacertret.668@gmail.com>
2025-06-08 20:24:02 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,27 +1,52 @@
-const CACHE_NAME = "playlist-cache-v1";
+const CACHE_NAME = "musicogris-cache-v2";
 const urlsToCache = [
-  "index.html",
-  "style.css",
-  "app.js",
-  "manifest.json",
-  "musica/cancion1.mp3",
-  "musica/cancion2.mp3",
-  "iconos/icon-192.png",
-  "iconos/icon-512.png"
+  "./",
+  "./index.html",
+  "./style.css",
+  "./manifest.json",
+  "./musica/cancion1.mp3",
+  "./musica/cancion2.mp3",
+  "./iconos/icon-192.png",
+  "./iconos/icon-512.png"
 ];
 
-self.addEventListener("install", event => {
+// Instalar: guardar todo en caché
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
+    caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(urlsToCache);
     })
   );
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", event => {
+// Activar: limpiar cachés antiguas
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// Buscar archivos: red primero, si no hay red, usar caché
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    fetch(event.request)
+      .then((response) => {
+        const responseClone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseClone);
+        });
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
